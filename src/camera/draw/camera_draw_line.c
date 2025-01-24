64d53e7ceee9fe04091b8d2aa3996da9e3d5eeae
Yumpty <julian.hemmer@epitech.eu>
2025-01-24 18:57:36 +0100

    [FIX] - Fix coding style error on camera draw line.

--- src/camera/draw/camera_draw_line.c
+++ src/camera/draw/camera_draw_line.c
@@ -9,6 +9,40 @@
 
 #include <SFML/Graphics.h>
 
+static void append_vertex(
+    sfVertexArray* line,
+    double x, double y,
+    sfColor color)
+{
+    sfVertexArray_append(line, (sfVertex){ { x, y }, color });
+}
+
+static void set_line_data(
+    sfVertexArray* line,
+    vector2f_t start_pos,
+    vector2f_t end_pos,
+    double thickness)
+{
+    sfVector2f direction = {
+        end_pos.x - start_pos.x,
+        end_pos.y - start_pos.y
+    };
+    float length = sqrt(direction.x * direction.x + direction.y * direction.y);
+    sfVector2f normal = { -(direction.y / length), (direction.x / length) };
+    sfVector2f of1 =
+        { normal.x * thickness / 2, normal.y * thickness / 2 };
+    sfVector2f of2 =
+        { -(normal.x) * thickness / 2, -(normal.y) * thickness / 2 };
+
+    sfVertexArray_setPrimitiveType(line, sfTriangles);
+    append_vertex(line, start_pos.x + of1.x, start_pos.y + of1.y, sfWhite);
+    append_vertex(line, start_pos.x + of2.x, start_pos.y + of2.y, sfWhite);
+    append_vertex(line, end_pos.x + of1.x, end_pos.y + of1.y, sfWhite);
+    append_vertex(line, end_pos.x + of1.x, end_pos.y + of1.y, sfWhite);
+    append_vertex(line, start_pos.x + of2.x, start_pos.y + of2.y, sfWhite);
+    append_vertex(line, end_pos.x + of2.x, end_pos.y + of2.y, sfWhite);
+}
+
 void camera_draw_line(
     pkm_camera_t *camera,
     vector2f_t start_position,
@@ -16,28 +50,11 @@ void camera_draw_line(
     double thickness)
 {
     sfRenderWindow *window = camera->game_info->window;
+    sfVertexArray* line = sfVertexArray_create();
+
     start_position = camera_convert_global_position(camera, start_position);
     end_position = camera_convert_global_position(camera, end_position);
-    sfVertexArray* line = sfVertexArray_create();
-    sfVector2f direction = { end_position.x - start_position.x, end_position.y - start_position.y };
-    float length = sqrt(direction.x * direction.x + direction.y * direction.y);
-
-    sfVertexArray_setPrimitiveType(line, sfTriangles);
-    if (length != 0) {
-        direction.x /= length;
-        direction.y /= length;
-    }
-    sfVector2f normal = { -direction.y, direction.x };
-    sfVector2f offset1 = { normal.x * thickness / 2, normal.y * thickness / 2 };
-    sfVector2f offset2 = { -normal.x * thickness / 2, -normal.y * thickness / 2 };
-
-    sfVertexArray_append(line, (sfVertex){ { start_position.x + offset1.x, start_position.y + offset1.y }, sfWhite });
-    sfVertexArray_append(line, (sfVertex){ { start_position.x + offset2.x, start_position.y + offset2.y }, sfWhite });
-    sfVertexArray_append(line, (sfVertex){ { end_position.x + offset1.x, end_position.y + offset1.y }, sfWhite });
-    sfVertexArray_append(line, (sfVertex){ { end_position.x + offset1.x, end_position.y + offset1.y }, sfWhite });
-    sfVertexArray_append(line, (sfVertex){ { start_position.x + offset2.x, start_position.y + offset2.y }, sfWhite });
-    sfVertexArray_append(line, (sfVertex){ { end_position.x + offset2.x, end_position.y + offset2.y }, sfWhite });
-    
+    set_line_data(line, start_position, end_position, thickness);
     sfRenderWindow_drawVertexArray(window, line, NULL);
     sfVertexArray_destroy(line);
 }
